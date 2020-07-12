d9fb06f114d6eee0edd1d3d06935e2e5549fbf3b
bruce2019 <rishi_masters@yahoo.com>
2020-07-12 21:37:48 +0530

    Scraping imdb via cheerio

---
+++ server.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var request = require('request');
+var cheerio = require('cheerio');
+var app     = express();
+
+app.get('/scrape', function(req, res){
+
+    url = 'http://www.imdb.com/title/tt0468569/';
+
+    request(url, function(error, response, html){
+        if(!error){
+            var $ = cheerio.load(html);
+
+            $('.title_wrapper').filter(function(){
+              var data = $(this);
+
+              var title = data.children().first().text();
+              console.log('title : ',title)
+
+              var release = data.children().next().text();
+              console.log('release : ',release)
+            })
+
+            $('.ratingValue').filter(function(){
+              var data = $(this);
+              var rating = data.children().text();
+              console.log('rating : ',rating)
+            })
+
+            $('.txt-block', '#titleDetails').filter(function(){
+              var data = $(this);
+
+              var value = data.children().text();
+              console.log('other details : ',value)
+            })
+        }
+        res.send('Check your console!')
+    })
+})
+
+app.listen('8081')
+console.log('Magic happens on port 8081');
+exports = module.exports = app;
